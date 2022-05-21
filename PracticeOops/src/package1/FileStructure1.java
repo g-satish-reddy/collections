a07591399f0fa69241fd3c205323f9f5f4017ee6
g-satish-reddy <gsatishreddy96@gmail.com>
2022-05-21 22:41:08 +0530

    oops_1

---
+++ PracticeOops/src/package1/FileStructure1.java
@@ -0,0 +1,28 @@
+package package1;
+
+class A
+{
+	public static void main(String[] args) {
+		System.out.println("From Class A");
+	}
+}
+	
+class B
+{
+	public static void main(String[] args) {
+		System.out.println("From Class B");
+	}
+	
+}
+
+class C
+{
+	public static void main(String[] args) {
+		System.out.println("From Class C");
+	}
+	
+}
+class D
+{
+	
+}
---
+++ PracticeOops/src/package1/Imports.java
@@ -0,0 +1,13 @@
+package package1;
+import java.util.*;
+
+public class Imports {
+	public static void main(String[] args) 
+	{
+		ArrayList a = new ArrayList();
+		String s = new String("Satish");
+		System.out.println(s);
+		
+	}
+
+}
